9688b0619915da56ae50c50a75427338f1517e3a
dsunwoo <sunwoo.daniel@gmail.com>
2016-07-15 17:44:24 -0500

    adding version2 of working code

---
+++ citibike_works2.py
@@ -0,0 +1,89 @@
+from pandas.io.json import json_normalize
+import requests
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+from dateutil.parser import parse
+import collections
+import sqlite3 as lite
+
+r = requests.get('https://feeds.citibikenyc.com/stations/stations.json')
+# Obtain keys
+key_list = [] # unique list of keys for each station listing
+for station in r.json()['stationBeanList']:
+    for k in station.keys():
+        if k not in key_list:
+            key_list.append(k)
+df = json_normalize(r.json()['stationBeanList'])
+
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+with con:
+    cur.executescript('DROP TABLE IF EXISTS citibike_reference')
+    cur.executescript('DROP TABLE IF EXISTS available_bikes')
+    # cur.execute('DELETE FROM available_bikes')
+    cur.execute('CREATE TABLE citibike_reference '
+                '(id INT '
+                'PRIMARY KEY, '
+                'totalDocks INT, '
+                'city TEXT, '
+                'altitude INT, '
+                'stAddress2 TEXT, '
+                'longitude NUMERIC, '
+                'postalCode TEXT, '
+                'testStation TEXT, '
+                'stAddress1 TEXT, '
+                'stationName TEXT, '
+                'landMark TEXT, '
+                'latitude NUMERIC, '
+                'location TEXT )'
+                )
+# a prepared SQL statement we're going to execute over and over again
+sql = "INSERT INTO citibike_reference " \
+      "(id, totalDocks, city, altitude, stAddress2, longitude, " \
+      "postalCode, testStation, stAddress1, stationName, landMark, latitude, location) " \
+      "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+# for loop to populate values in the database
+with con:
+    for station in r.json()['stationBeanList']:
+        # id, totalDocks, city, altitude, stAddress2, longitude, postalCode,
+        # testStation, stAddress1, stationName, landMark, latitude, location
+        cur.execute(sql,
+                    (station['id'],
+                     station['totalDocks'],
+                     station['city'],
+                     station['altitude'],
+                     station['stAddress2'],
+                     station['longitude'],
+                     station['postalCode'],
+                     station['testStation'],
+                     station['stAddress1'],
+                     station['stationName'],
+                     station['landMark'],
+                     station['latitude'],
+                     station['location'])
+                    )
+# extract the column from the DataFrame and put them into a list
+station_ids = df['id'].tolist()
+# add the '_' to the station name and also add the data type for SQLite
+station_ids = ['_' + str(x) + ' INT' for x in station_ids]
+# create the table
+# in this case, we're concatenating the string and joining all the station ids (now with '_' and 'INT' added)
+with con:
+    cur.execute("CREATE TABLE available_bikes ( execution_time INT, " + ", ".join(station_ids) + ");")
+# take the string and parse it into a Python datetime object
+
+for i in range(60):
+    r = requests.get('https://feeds.citibikenyc.com/stations/stations.json')
+    exec_time = parse(r.json()['executionTime']).strftime("%s")
+
+    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time,))
+
+    for station in r.json()['stationBeanList']:
+        cur.execute("UPDATE available_bikes SET _%d = %d WHERE execution_time = %s" % (station['id'], station['availableBikes'], exec_time))
+    con.commit()
+
+    time.sleep(60)
+
+con.close()  # close the database connection when done
